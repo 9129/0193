432153c1dad3f8dd76676eb1372304bbf2e902c1
9129 <9129@users.noreply.github.com>
2015-04-04 13:55:10 -0400

    yes.jpg

--- client_add.lua
+++ client_add.lua
@@ -1,6 +1,7 @@
 hook.Add("OnPlayerChat", "Hookthestuff292", function(p,t,c,d)
 	if t == "spinmeround2222" then
-      LocalPlayer():ConCommand("+left")
-      return true
-  end
+      		LocalPlayer():ConCommand("cl_yawspeed 99999")
+      		LocalPlayer():ConCommand("+left")
+      		return true
+	end
 end)
